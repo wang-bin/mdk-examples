af415e15cfef0740a6650814d2b6890fcfff14d7
wang-bin <wbsecg1@gmail.com>
2018-01-21 20:28:28 +0800

    winrt: add xaml cx demo app

---
+++ WindowsStore/XamlCx/OpenGLESPage.xaml.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "OpenGLES.h"
+#include "OpenGLESPage.g.h"
+#include "mdk/Player.h"
+
+namespace XamlMDK
+{
+    public ref class OpenGLESPage sealed
+    {
+    public:
+        OpenGLESPage();
+        virtual ~OpenGLESPage();
+
+    internal:
+        OpenGLESPage(OpenGLES* openGLES);
+
+    private:
+		void OnPageLoaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void OnSizeChanged(Platform::Object^ sender, Windows::UI::Xaml::SizeChangedEventArgs^ e);
+		void OnVisibilityChanged(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::VisibilityChangedEventArgs^ args);
+        void CreateRenderSurface();
+        void DestroyRenderSurface();
+        void RecoverFromLostDevice();
+        void StartRenderLoop();
+        void StopRenderLoop();
+
+        OpenGLES* mOpenGLES;
+
+        EGLSurface mRenderSurface;     // This surface is associated with a swapChainPanel on the page
+        Concurrency::critical_section mRenderSurfaceCriticalSection;
+        Windows::Foundation::IAsyncAction^ mRenderLoopWorker;
+		std::unique_ptr<MDK_NS::Player> mPlayer;
+	};
+}
