0248c2cdd4b7aefb8fbc18b9eb21637d6ddc171e
wang-bin <wbsecg1@gmail.com>
2018-01-28 13:24:55 +0800

    add internally created window example

---
+++ window/README.md
@@ -0,0 +1,9 @@
+# MDK Internally Created Window
+
+Platforms
+
+- Win32 desktop
+- X11
+- Wayland
+- GBM
+- Raspberry Pi brcm dispmanx
---
+++ window/mdkplay.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016-2018 WangBin <wbsecg1 at gmail.com>
+ * MDK SDK with internal created window example
+ */
+#include <cstring>
+#include <mdk/Player.h>
+using namespace MDK_NS;
+using namespace std;
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        printf("usage: %s [-c:v decoder] [-size widthxheight] url\n", argv[0]);
+        return 0;
+    }
+    Player p;
+    p.createWindow(); // windows, x11, wayland, gbm, rpi 
+    p.resizeWindow(1280, 720);
+    for (int i = 1; i < argc; ++i) {
+        if (std::strcmp(argv[i], "-c:v") == 0) {
+            p.setVideoDecoders({argv[++i]});
+        } else if(std::strcmp(argv[i], "-size") == 0) {
+            int w = 1920, h=1080;
+            sscanf(argv[++i], "%dx%d", &w, &h);
+            p.resizeWindow(w, h); 
+        }
+    }
+    p.setMedia(argv[argc-1]);
+    p.setState(State::Playing);
+    p.showWindow();
+    return 0;
+}
