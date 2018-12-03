aea109670011ebcf6c986d1b959823f488729caa
wang-bin <wbsecg1@gmail.com>
2018-12-03 18:18:28 +0800

    uwp: add open file dialog

--- WindowsStore/XamlCx/OpenGLESPage.xaml.cpp
+++ WindowsStore/XamlCx/OpenGLESPage.xaml.cpp
@@ -1,11 +1,19 @@
 ﻿#include "pch.h"
 #include "OpenGLESPage.xaml.h"
 #include "mdk/Player.h"
+#include <locale>
+#include <codecvt>
 #include <Inspectable.h>
-
+#include <collection.h>
+#include <ppltasks.h>
+using namespace concurrency;
 using namespace std;
 using namespace MDK_NS;
 using namespace XamlMDK;
+
+using namespace Windows::Foundation::Collections;
+using namespace Windows::Storage;
+using namespace Windows::Storage::Pickers;
 
 static struct init {
 	init() {
@@ -32,10 +40,10 @@ OpenGLESPage::~OpenGLESPage()
 
 void OpenGLESPage::OnPageLoaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
+
 	mPlayer->updateNativeWindow(reinterpret_cast<IInspectable*>(swapChainPanel_0));
-	mPlayer->setVideoDecoders({ "D3D11", "FFmpeg" });
-	mPlayer->setMedia("rtmp://live.hkstv.hk.lxdns.com/live/hks");
-	mPlayer->setState(PlaybackState::Playing);
+	mPlayer->setVideoDecoders({ "MFT:d3d=11", "D3D11", "FFmpeg" });
+    mPlayer->setMedia("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov");
 }
 
 void OpenGLESPage::OnVisibilityChanged(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::VisibilityChangedEventArgs^ args)
@@ -59,4 +67,30 @@ void OpenGLESPage::OnModeSelected(Platform::Object^ sender, Windows::UI::Xaml::R
 	if (!swapChainPanel_1)
 		return;
 	swapChainPanel_1->Visibility = vis;
+}
+
+void OpenGLESPage::OnSelectFiles(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs e)
+{
+  FileOpenPicker^ openPicker = ref new FileOpenPicker();
+  openPicker->ViewMode = PickerViewMode::List;
+  openPicker->SuggestedStartLocation = PickerLocationId::DocumentsLibrary;
+  openPicker->FileTypeFilter->Append("*");
+  create_task(openPicker->PickMultipleFilesAsync()).then([this](IVectorView<StorageFile^>^ files) {
+    if (files->Size > 0) {
+      std::string filename;
+      for (auto file : files) { // TODO: play list
+        auto filenamew = file->Path;
+        wstring_convert<codecvt_utf8<wchar_t>> cvt;
+        filename = cvt.to_bytes(filenamew->Data());
+        mFile = file;
+      }
+      auto ptr = reinterpret_cast<IInspectable*>(mFile);
+      OutputDebugStringA(filename.data());
+      auto url = std::string("winrt:IStorageFile@").append(std::to_string((long long)ptr));
+      mPlayer->setState(State::Stopped);
+      mPlayer->waitFor(State::Stopped);
+      mPlayer->setMedia(url.data());
+      mPlayer->setState(PlaybackState::Playing);
+    }
+  });
 }
