781c2982872ede940c6bdda711fd23647edb978f
wang-bin <wbsecg1@gmail.com>
2023-08-05 22:58:30 +0800

    flutter: single player multiple textures example

--- flutter/simple/README.md
+++ flutter/simple/README.md
@@ -1,3 +1,6 @@
 # simple
 
-A simple Flutter video_player example with fvp plugin to support all platforms.
+examples are based on [fvp](https://pub.dev/packages/fvp) package
+
+- main.dart: flutter video_player example using fvp plugin
+- multi_textures: a single player display in multiple Textures via fvp player api
---
+++ flutter/simple/lib/multi_textures.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:fvp/mdk.dart' as mdk;
+
+void main(List<String> args) async {
+  runApp(const SinglePlayerMultipleVideoWidget());
+}
+class SinglePlayerMultipleVideoWidget extends StatefulWidget {
+  const SinglePlayerMultipleVideoWidget({Key? key}) : super(key: key);
+
+  @override
+  State<SinglePlayerMultipleVideoWidget> createState() =>
+      _SinglePlayerMultipleVideoWidgetState();
+}
+
+class _SinglePlayerMultipleVideoWidgetState
+    extends State<SinglePlayerMultipleVideoWidget> {
+  late final player = mdk.Player();
+
+  @override
+  void initState() {
+    super.initState();
+
+    player.media = 'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4';
+    player.loop = -1;
+    player.state = mdk.State.playing;
+    player.updateTexture();
+  }
+
+  @override
+  void dispose() {
+    player.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'package:fvp',
+      home: Scaffold(
+        body: Center(
+          child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Expanded(
+                          child: Card(
+                            elevation: 8.0,
+                            color: Colors.black,
+                            clipBehavior: Clip.antiAlias,
+                            margin: const EdgeInsets.all(32.0),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Expanded(
+                                  child: Row(
+                                    children: [
+                                      Expanded(
+                                        child: ValueListenableBuilder<int?>(
+                                          valueListenable: player.textureId,
+                                          builder: (context, id, _) => id == null
+                                            ? const SizedBox.shrink()
+                                            : Texture(textureId: id),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child:  ValueListenableBuilder<int?>(
+                                          valueListenable: player.textureId,
+                                          builder: (context, id, _) => id == null
+                                            ? const SizedBox.shrink()
+                                            : Texture(textureId: id),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Row(
+                                    children: [
+                                      Expanded(
+                                        child: ValueListenableBuilder<int?>(
+                                          valueListenable: player.textureId,
+                                          builder: (context, id, _) => id == null
+                                            ? const SizedBox.shrink()
+                                            : Texture(textureId: id),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child:  ValueListenableBuilder<int?>(
+                                          valueListenable: player.textureId,
+                                          builder: (context, id, _) => id == null
+                                            ? const SizedBox.shrink()
+                                            : Texture(textureId: id),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 32.0),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+        ),
+      ),
+    );
+  }
+}
