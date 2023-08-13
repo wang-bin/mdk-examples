0885d707e087955c6ed85fe40ed7c4606c0713c7
wang-bin <wbsecg1@gmail.com>
2023-08-13 22:17:23 +0800

    decodefps: loop option

--- Native/DecodeFps.cpp
+++ Native/DecodeFps.cpp
@@ -1,5 +1,6 @@
 #include "mdk/Player.h"
 #include "mdk/VideoFrame.h"
+//#include "mdk/MediaInfo.h"
 #include <chrono>
 #include <cstring>
 #include <future>
@@ -19,20 +20,23 @@ int main(int argc, const char** argv)
         }
     }
 
-    VideoFrame v;
+    const VideoFrame v;
     int64_t from = 0;
     bool decode1 = false;
+    int loop = 0, l = 0;
     Player p;
     for (int i = 1; i < argc; ++i) {
         if (std::strcmp(argv[i], "-c:v") == 0)
             p.setDecoders(MediaType::Video, {argv[++i]});
         else if (std::strcmp(argv[i], "-from") == 0)
             from = std::atoi(argv[++i]);
-        else if (std::strcmp(argv[i], "-one") == 0) {
+        else if (std::strcmp(argv[i], "-one") == 0)
             decode1 = true;
-        }
+        else if (std::strcmp(argv[i], "-loop") == 0)
+            loop = std::atoi(argv[++i]);
     }
     p.setMedia(argv[argc-1]);
+    p.setLoop(loop);
     // only the 1st video track will be decoded
     p.setDecoders(MediaType::Audio, {});
     p.onSync([]{return DBL_MAX;}); // do not sync to present time
@@ -46,8 +50,11 @@ int main(int argc, const char** argv)
     p.onFrame<VideoFrame>([&](VideoFrame& v, int){
         if (!v || v.timestamp() == TimestampEOS) { // AOT frame(1st frame, seek end 1st frame) is not valid, but format is valid. eof frame format is invalid
             printf("decoded: %d, elapsed: %" PRId64 ", fps: %.1f/%.1f\n", count, elapsed, count*1000.0/elapsed, fps);
-            printf("invalid frame. eof?\n");
-            pm.set_value(0);
+            l += v && v.timestamp() == TimestampEOS;
+            printf("invalid frame %d. eof? loop=%d/%d\n", v.isValid(), l, loop);
+            if (loop >= 0 && l > loop) {
+                pm.set_value(0);
+            }
             return 0;
         }
         if (!v.format()) {
@@ -75,8 +82,13 @@ int main(int argc, const char** argv)
             pm.set_value(-1);
         return true;
     });
+    //p.waitFor(State::Paused);
+    //printf("video format: %d - %s\n", p.mediaInfo().video[0].codec.format, p.mediaInfo().video[0].codec.format_name);
     p.set(State::Running);
+    //this_thread::sleep_for(1s);
+    //p.set(State::Stopped);
     const auto ret = fut.get();
     printf("frame count: %d, render count: %d\n", count, rcount);
+    //p.waitFor(State::Stopped);
     return ret;
 }
